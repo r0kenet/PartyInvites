f3eee52202d4486a149fb37353e617e28b83bdf2
Eirik Horvath <eirik@geta.no>
2016-07-06 15:58:56 +0200

    Added RSVP Sign up form.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -15,5 +15,11 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
+
+        public ViewResult RsvpForm()
+        {
+
+            return View();
+        }
     }
 }
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -10,6 +10,6 @@ namespace PartyInvites.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public bool? WillAttend { get; set; }
+        public bool WillAttend { get; set; }
     }
 }
